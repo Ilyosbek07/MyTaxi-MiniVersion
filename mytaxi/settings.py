1e311a18e740c22c88ecaf9a7047d131a256d421
Ilyosbek <phyton@micro.domains>
2022-01-13 23:52:20 +0500

    update

--- mytaxi/settings.py
+++ mytaxi/settings.py
@@ -79,25 +79,18 @@ WSGI_APPLICATION = 'mytaxi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mytaxi_db',
+        'USER': 'Ilyosbek',
+        'PASSWORD': 'db_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'db_db',
-        # 'USER': 'db_user',
-        # 'PASSWORD': 'db_password',
-        # 'HOST': 'localhost',
-        # 'PORT': '3306',
-    # }
-# }
-# 114177777
-# dsdsdas
-# Password validation
+ # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
